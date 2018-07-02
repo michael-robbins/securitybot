0db4a6873494a7895e8f240af7043d271abf9d60
Michael Robbins <michael@dalmura.com.au>
2018-07-02 22:57:12 +1000

    Adding in setup files for packaging

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SecurityBot",
+    version="0.0.1",
+    description="The glue between human and security interfaces",
+    url="https://github.com/michael-robbins/securitybot",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Communications :: Chat",
+        "Topic :: Security",
+        "Topic :: Home Automation",
+    ],
+    keywords="slack slackbot zoneminder security",
+    license="GNU",
+    packages=find_packages(),
+    install_requires=[
+        "pyyaml>=3,<4",
+        "slackclient>=1.1,<2",
+    ],
+    python_requires="~=3.5",
+    extras_require={
+        "test": ["pytest>=3.3,<4"],
+    }
+)
